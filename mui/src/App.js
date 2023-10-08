743045fd8f09096d6d9ec2d38cddb98728efc6a9
Salman <salmanh084@gmail.com>
2023-10-08 02:42:07 -0500

    starting files

---
+++ mui/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Button from '@mui/material/Button';
+import Navbar from './components/Navbar/Navbar';
+function App() {
+  
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
